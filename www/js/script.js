a896151ec1448676551f46921ae32c04d291645f
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2019-10-13 23:20:11 +0100

    Added all last pages,footer and added stickytop nav

--- www/js/script.js
+++ www/js/script.js
@@ -1,9 +1,14 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 window.addEventListener('load', (event) => {
   const svg = document.querySelectorAll('.svg');
 
+
   for (let i = 0; i < svg.length; i++) {
     var x = 0;
-    if (i == 22){
+    if (i == 22 || i == svg.length-1){
       svg[i].classList.add("animated");
       svg[i].classList.add("bounceInDown");
       x = i / 40;
@@ -22,15 +27,36 @@ window.addEventListener('load', (event) => {
   }
 });
 
-var btns = document.querySelectorAll('shopInfoTitle');
+var btns = document.querySelectorAll('.shopInfoTitle');
+var info = document.querySelectorAll('.shopInfoText');
+
+
 
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function(){
-    console.log("success");
-    // if(btns[i].classList != "active"){
-      
-    //   btns[i].classList.add(" active");
+  btns[i].addEventListener('click', function(idNum){
+    var current = document.getElementsByClassName('active');
+    if(current[0].id == btns[i].id){
+      current[0].className = current[0].className.replace(' active underLine', '');
+    }
+    else{
+      for (let x = 0; x < current.length; x++) {
+        if(current[x].id == btns[i].id){
+          current[x].className = current[x].className.replace(' active underLine', '');
+          
+        }
+      }
+    }
+    btns[i].className += ' active underLine';
 
-    // }
-  });
+    for (let u = 0; u < info.length; u++) {
+      info[u].classList.add('hidden');
+      }
+    info[i].classList.remove('hidden');
+   });
 } 
+
+
+
+
+
+
