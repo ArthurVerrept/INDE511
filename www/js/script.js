3cbf55a229c347e4a6bc9a645f17c9451f9f14e3
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2019-10-21 15:14:34 +0100

    final commit added comments and fixed bugs

--- www/js/script.js
+++ www/js/script.js
@@ -1,50 +1,73 @@
+// setting scroll to very top of page before load
 window.onbeforeunload = () => {
   window.scrollTo(0, 0);  
 }
 
+// when burger menu button is pressed in 
 function openNav() {
+  // set nav background to fill the whole screen
   document.getElementById("myNav").style.height = "100%";
+  // show close menu button
   document.querySelector(".burgerClosed").classList.replace('hidden', 'unHidden');
-  document.querySelector(".burgerClosed").classList.add = ' animated fadeOut';
 }
 
+// when close burger menu button is pressed
 function closeNav() {
+  // set height to 0
   document.getElementById("myNav").style.height = "0%";
-
+  // hide close burger menu button
   document.querySelector(".burgerClosed").classList.replace('unHidden', 'hidden');
 }
 
-window.addEventListener('scroll', ()=> {
+
+// listening for scroll 
+window.addEventListener('scroll', () => {
+  // set scroll top variable
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  // if 10 pixels off the top 
   if(scrollTop > 10){
+    // scale down the whole navbar
     document.querySelector('.navbar').style.transform = 'scale(0.9)';
+    // move navbar up to close space above
     document.querySelector('.navbar').style.top = '-1vw';
   }
   else{
+    // if not scale back to normal
     document.querySelector('.navbar').style.transform = 'scale(1)';
+    // reset position
     document.querySelector('.navbar').style.top = '0';
   }
-  
 });
 
 
+// when screen loads load 
 window.addEventListener('load', (event) => {
+  // select all elements with class of svg
   const svg = document.querySelectorAll('.svg');
 
-
+  // looping through each element
   for (let i = 0; i < svg.length; i++) {
+    // setting variable for use in maths later
     var x = 0;
+    // selecting the end of each svg text to animate differently
     if (i == 22 || i == svg.length-1){
+      // adding animate.css classes
       svg[i].classList.add('animated');
       svg[i].classList.add('bounceInDown');
+      // this takes current position and divides by 40 to increment start time of animation
       x = i / 40;
+      // converts time to string
       svg[i].style.animationDelay = x.toString()+'s';
+      // setting animation duration
       svg[i].style.animationDuration = '0.5s';
+      // setting fill colour
       svg[i].style.fill = '#65433D';
     }
     else{
+      // for every other letter do a pulse animation
       svg[i].classList.add('animated');
       svg[i].classList.add('pulse');
+      // setting start time
       x = i / 40;
       svg[i].style.animationDelay = x.toString()+'s';
       svg[i].style.animationDuration = '0.3s';
@@ -54,7 +77,6 @@ window.addEventListener('load', (event) => {
 });
 
 var btns = document.querySelectorAll('.shopInfoTitle');
-var info = document.querySelectorAll('.shopInfoText');
 
 
 
@@ -74,10 +96,6 @@ for (let i = 0; i < btns.length; i++) {
     }
     btns[i].className += ' active underLine';
 
-    for (let u = 0; u < info.length; u++) {
-      info[u].classList.add('hidden');
-      }
-    info[i].classList.remove('hidden');
    });
 } 
 
